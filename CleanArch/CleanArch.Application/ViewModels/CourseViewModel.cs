dcb1d0d744706dc246b3be32b361116e09b2f807
Ioana Bianca Blaga <ioanabianca.blaga@gilead.com>
2021-01-25 17:46:12 +0200

    commit Service and Repositry Interfaces, CourseViewModel

---
+++ CleanArch/CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        /// <summary>
+        /// Lista de cursuri.
+        /// Face legatura cu Domain Layer
+        /// </summary>
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
