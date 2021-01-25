76be622b0b430cb3281990f1e805cbec4dcfdc25
Ioana Bianca Blaga <ioanabianca.blaga@gilead.com>
2021-01-25 18:38:04 +0200

    Confogure Presentation Layer for Dependency Container

---
+++ CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        //here will register evreything
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            // Data Layer - Infrastructure Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
