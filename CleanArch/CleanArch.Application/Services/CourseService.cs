449a75db421d46898d76104a70684fe26a02584b
Ioana Bianca Blaga <ioanabianca.blaga@gilead.com>
2021-01-25 18:14:42 +0200

    commit implementation of Serivice and Repository

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        //2. make a reference to repository 
+        private ICourseRepository _courseRepository;
+
+        //3. create a constructor in which inject the repo
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            //1. make a call to the repository
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
